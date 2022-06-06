8750a05294a26dbcc322b7f67d5a3f69b3bc8c94
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2022-06-06 08:17:35 -0700

    Simplify compilation script

--- compile.sh
+++ compile.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 # Compile jq to WebAssembly
+
 # Need Emscripten installed
+[[ ! $(command -v emcc --version) ]] && \
+  echo "You need Emscripten to compile jq to WebAssembly. See the setup instructions in the README file." && \
+  exit
 
 # Update jq repo and its submodules
 cd jq
-git submodule update --init
-git submodule update --init
+git submodule update --init --recursive
 
 # Edit main.c so that reset option flags to 0 every time we call the main() function
 # This is needed because we're not exiting after each main() call; otherwise, the
 # "Sort Keys" feature wouldn't work
-MAIN_FN="int main(int argc, char\* argv\[\]) {"
-sed -i "s/${MAIN_FN}/${MAIN_FN} options = 0; /" src/main.c | grep "int main"
+git apply ../jq.patch
 
 # Generate ./configure file
 autoreconf -fi
@@ -22,16 +24,10 @@ emconfigure ./configure \
     --with-oniguruma=builtin \
     --disable-maintainer-mode
 
-# Build jq executable
-emmake make LDFLAGS=-all-static
+# Compile jq and generate .js/.wasm files
+emmake make \
+  EXEEXT=.js \
+  CFLAGS="-O2 -s EXPORTED_RUNTIME_METHODS=['callMain']"
 
-# Convert to .o; otherwise emcc complains
-# that the "file has an unknown suffix"
-cp jq ../jq.o
-cd ..
-
-# Compile to WebAssembly (we need .js/.wasm files; we can't execute jq.o in the browser)
-# - Use "ERROR_ON_UNDEFINED_SYMBOLS=0" to ignore "undefined symbol: llvm_fma_f64" warning
-emcc jq.o -o jq.js \
-  -s EXTRA_EXPORTED_RUNTIME_METHODS='["callMain"]' \
-  -s ERROR_ON_UNDEFINED_SYMBOLS=0
+mkdir -p ../build/
+mv jq.{js,wasm} ../build/
